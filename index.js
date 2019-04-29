cb665816e7e3a6d71ee9b7f46fe4f398ad079782
claytongulick <claytongulick@gmail.com>
2019-04-29 10:29:49 -0500

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import {test} from 'json-predicate';
 
 class JSONPatchRules {
-    constructor(rules) {
+    constructor(rules, options) {
+        this.options = Object.assign({
+            mode: 'whitelist'
+        },options);
         this.rules = rules;
     }
 
@@ -13,7 +16,68 @@ class JSONPatchRules {
     }
 
     checkOperation(item) {
+        let rules = this.findRules(item);
+        if(!rules || (rules.length == 0))
+        {
+            //we didn't find a rule. depending on whether we're in whitelist or blacklist mode, this might be permissable
+            if(this.options.mode == 'whitelist')
+                return false;
+            return true;
+        }
+    }
 
+    findRules(item) {
+        return this.rules.filter(
+            (rule) => {
+                return this.ruleMatches(item, rule);
+            }
+        );
+    }
+
+    ruleMatches(item, rule) {
+
+        if(rule.path) {
+            let path = rule.path;
+            //if this is a regex
+            if(path.indexOf('^/') == 0) {
+                let regex = new RegExp(path);
+                let match = regex.test(item.path);
+                if(!match)
+                    return false;
+            }
+            else if(path != item.path) {
+                return false;
+            }
+        }
+
+        if(rule.op) {
+            if(Array.isArray(rule.op)) {
+                let op_match = rule.op.includes(item.op)
+                if(!op_match)
+                    return false;
+            }
+            else {
+                if(!rule.op === item.op)
+                    return false;
+            }
+        }
+
+        if(rule.test) {
+            //if this is an array, it's a json predicate set
+            if(Array.isArray(rule.test)) {
+                let test_match = test(item, rules.test);
+                if(!test_match)
+                    return false;
+            }
+            else {
+                let regex = new RegExp(rule.test);
+                let test_match = regex.test(item.value);
+                if(!test_match)
+                    return false;
+            }
+        }
+
+        return true;
     }
 
 }
